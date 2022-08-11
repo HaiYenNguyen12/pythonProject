e66203326689e38318530d70a4bb44282db74e83
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2022-08-11 23:54:06 +0700

    CRUD and Search

--- discord/base/urls.py
+++ discord/base/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from base.views import delete_view, home_view, create_room , room_view, delete_view
+from base.views import delete_view, home_view, create_room , room_view, delete_view, update_room
 urlpatterns = [
-    path('', home_view),
-    path('<int:id>/', room_view, name="room"),
-    # path('room/<int:id>/', room_view,name="room"),
-    path('create', create_room),
-    path('delete/<int:id>', delete_view),
+    path('', home_view, name="home"),
+    path('<str:pk>/', room_view, name="room"),
+    path('update/<str:pk>/', update_room,name="update_room"),
+    path('create', create_room, name="create_room"),
+    path('delete/<str:pk>', delete_view, name = "delete_room"),
 ]
--- discord/base/views.py
+++ discord/base/views.py
@@ -1,32 +1,76 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic
+from .forms import RoomForm
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    qs = Room.objects.all()
+    q = request.GET.get("q")
+    if q != None:
+        qs = Room.objects.filter(topic__name__icontains=q)
+    else:
+        qs = Room.objects.all()
+    qs_topic = Topic.objects.all()
     if qs.exists():
         context = {
-            "room_list" : qs
+            "room_list" : qs,
+            "topics" : qs_topic
         }
         return render(request, "base/home.html", context, status = 200)
 
-def room_view(request, id, *args, **kwargs):
-    qs = Room.objects.filter(id = id)
+def room_view(request, pk, *args, **kwargs):
+    qs = Room.objects.filter(id = pk)
     if qs.exists():
         context = {
             "room" : qs.first()
         }
         return render(request, "base/room.html", context, status = 200)
 
-def delete_view (request, id, *args, **kwargs ):
+def delete_view (request, pk, *args, **kwargs ):
     qs = Room.objects.all()
-    qs.delete(id= id)
+    qs.delete(id = pk)
     qs.save()
     context = {
     "room_list" : qs
     }
-    redirect("")
+    return redirect("home")
 
 def create_room (request, *args, **kwargs):
+    form  = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {
+        "form" : form
+    }
     
-    return render(request,"base/room_form.html", context={"mess":"hello world"})
+    return render(request,"base/room_form.html", context)
+
+
+def update_room (request, pk, *args, **kwargs ):
+    qs = Room.objects.get(id = pk)
+    form  = RoomForm(instance=qs)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=qs)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {
+        "form" : form
+    }
+    
+    return render(request,"base/room_form.html", context)
+
+
+    
+def delete_view(request, pk, *args, **kwargs):
+    obj = Room.objects.get(id=pk)
+    if request.method == "POST":
+        obj.delete()
+        return redirect("home")
+
+    return render(request,"base/delete.html", context={"room" : obj})
